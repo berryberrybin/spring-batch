415c899ccac7be98f8dbf28e499202a6efdfd6d4
berryberrybin <67494127+berryberrybin@users.noreply.github.com>
2022-08-01 11:05:02 +0900

    first commit

---
+++ src/main/java/com/example/springbatch1/Springbatch1Application.java
@@ -0,0 +1,13 @@
+package com.example.springbatch1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbatch1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbatch1Application.class, args);
+	}
+
+}
